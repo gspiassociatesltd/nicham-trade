ecdb7c2432528c0c0eb6d1251963cac6e575ef88
Godwin Abaniwo <gspiassociatesltd@gmail.com>
2026-09-11 08:49:25 +0100

    Add files via upload

--- components/VoiceOrder.tsx
+++ components/VoiceOrder.tsx
@@ -59,7 +59,7 @@ const productKeywords: any = {
   pcm: { bike: 1, cargo: 1, keke: 1, pump: 2, water: 2, freezer: 3, cold: 3, grid: 4, mini: 4, tractor: 5, dryer: 6, canoe: 7, boat: 8, foldable: 9 },
   ha: { keke: 1, kaya: 1, bike: 1, famfo: 2, ruwa: 2, pump: 2, firiza: 3, sanyi: 3, grid: 4, mini: 4, tarikta: 5, noma: 5, bushewa: 6, kwale: 7, jirgi: 8, ninkewa: 9 },
   ig: { igwe: 1, ibu: 1, bike: 1, mmiri: 2, pump: 2, friza: 3, grid: 4, trakto: 5, nkucha: 6, ugbo: 7, mpiachi: 9, fold: 9 },
-  yo: { keke: 1, eru: 1, bike: 1, omi: 2, fompu: 2, firisa: 3, grid: 4, tirakito: 5, gbigbe: 6, oko: 7, omi: 8, kika: 9, fold: 9 }
+  yo: { keke: 1, eru: 1, bike: 1, omi_eto: 2, fompu: 2, firisa: 3, grid: 4, tirakito: 5, gbigbe: 6, oko_omi: 7, oko_eru: 8, kika: 9, fold: 9 }
 }
 
 export default function VoiceOrder({ lang, products }: { lang: string, products: any }) {
